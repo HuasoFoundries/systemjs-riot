7d9eb65c63197643fa6bd4c20af441dc12afce8e
Amenadiel <amenadiel@gmail.com>
2016-04-14 19:25:38 -0300

    trying to use ES6

--- tag.js
+++ tag.js
@@ -1,6 +1,24 @@
-var compiler = require('riot-compiler');
+'use strict';
 
-exports.translate = function(load) {
+import compiler from 'riot-compiler';
+
+const riotRuntimePath = System.normalizeSync('riot', __moduleName);
+
+export function translate(load) {
+    var options = {
+        //expr: true
+        //type: 'babel'
+    };
+    load.metadata.format = 'amd';
+    var precompiled = compiler.compile(load.source, options);
+    var output = `define(['${riotRuntimePath}'], function(riot) { ${precompiled} });`;
+
+    load.source = output; // `module.exports = require('${handlebarsRuntimePath}').template(${precompiled});`;
+    return output;
+}
+
+
+/*exports.translate = function(load) {
     // @todo be able to pass the options in config.js
     var options = {
         //expr: true
@@ -8,9 +26,19 @@ exports.translate = function(load) {
     };
 
     load.metadata.format = 'amd';
-    var compiledtag = compiler.compile(load.source, options);
-    //    console.log(compiledtag);
-    var output = "def" + "ine(['riot'], function(riot) {" + "\n" + compiledtag + "\n" + "});";
 
-    return output;
+
+    return System.import('riot-compiler', {
+        name: module.id
+    }).then(function(compiler) {
+        console.log('compiler imported', compiler);
+        var compiledtag = compiler.compile(load.source, options);
+        //    console.log(compiledtag);
+        var output = "def" + "ine(['riot'], function(riot) {" + "\n" + compiledtag + "\n" + "});";
+        return output;
+    });
+
+
+
 };
+*/
